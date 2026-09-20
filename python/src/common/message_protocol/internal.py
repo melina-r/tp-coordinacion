65ea8682a99a6b13b519c04b033d4f81cfe21948
Melina <mretamozo@fi.uba.ar>
2026-09-20 10:51:41 -0300

    feat: add mom + internal serialization

--- python/src/common/message_protocol/internal.py
+++ python/src/common/message_protocol/internal.py
@@ -1,9 +1,63 @@
 import json
+from enum import Enum
 
+AMOUNT_SIZE = 4
+LENGTH_SIZE = 2
+TYPE_SIZE = 1
+
+class FieldType(Enum):
+    EndOfMessage = 0
+    FruitName = 1
+    FruitAmount = 2
+
+    def to_bytes(self):
+        return self.value.to_bytes(TYPE_SIZE, "big")
+
+def serialize_fruit_name(fruit_name):
+    bytes_value = fruit_name.encode("utf-8")
+    length = len(bytes_value)
+    data = b""
+    data += FieldType.FruitName.to_bytes()
+    data += length.to_bytes(LENGTH_SIZE, "big")
+    data += bytes_value
+    return data
+
+def serialize_fruit_amount(fruit_amount):
+    bytes_value = fruit_amount.to_bytes(AMOUNT_SIZE, "big")
+    length = len(bytes_value)
+    data = b""
+    data += FieldType.FruitAmount.to_bytes()
+    data += length.to_bytes(LENGTH_SIZE, "big")
+    data += bytes_value
+    return data
 
 def serialize(message):
-    return json.dumps(message).encode("utf-8")
+    if len(message) == 0:
+        return FieldType.EndOfMessage.to_bytes() + (0).to_bytes(LENGTH_SIZE, "big")
+        
+    fruit, amount = message
+    fruit_data = serialize_fruit_name(fruit)
+    amount_data = serialize_fruit_amount(amount)
+    total_length = len(fruit_data) + len(amount_data)
+    data = b""
+    data += total_length.to_bytes(LENGTH_SIZE, "big")
+    data += fruit_data
+    data += amount_data
+    return data
 
+def deserialize_fruit_name(data):
+    length = int.from_bytes(data[TYPE_SIZE:TYPE_SIZE + LENGTH_SIZE], "big")
+    fruit_name = data[TYPE_SIZE + LENGTH_SIZE : TYPE_SIZE + LENGTH_SIZE + length].decode("utf-8")
+    return fruit_name, TYPE_SIZE + LENGTH_SIZE + length
+
+def deserialize_fruit_amount(data):
+    length = int.from_bytes(data[TYPE_SIZE:TYPE_SIZE + LENGTH_SIZE], "big")
+    fruit_amount = int.from_bytes(data[TYPE_SIZE + LENGTH_SIZE : TYPE_SIZE + LENGTH_SIZE + length], "big")
+    return fruit_amount, TYPE_SIZE + LENGTH_SIZE + length
 
 def deserialize(message):
-    return json.loads(message.decode("utf-8"))
+    total_length = int.from_bytes(message[:LENGTH_SIZE], "big")
+    data = message[LENGTH_SIZE:LENGTH_SIZE + total_length]
+    fruit_name, offset = deserialize_fruit_name(data)
+    fruit_amount, _ = deserialize_fruit_amount(data[offset:])
+    return fruit_name, fruit_amount
--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -1,14 +1,121 @@
 import pika
-import random
-import string
-from .middleware import MessageMiddlewareQueue, MessageMiddlewareExchange
 
-class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
+from .middleware import (
+    MessageMiddlewareCloseError,
+    MessageMiddlewareDisconnectedError,
+    MessageMiddlewareExchange,
+    MessageMiddlewareMessageError,
+    MessageMiddlewareQueue,
+)
 
+
+class _MessageMiddlewareRabbitMQBase:
+    def __init__(self, host):
+        self.host = host
+        self.queue_name = None
+        try:
+            self.connection = pika.BlockingConnection(
+                pika.ConnectionParameters(host=host)
+            )
+            self.channel = self.connection.channel()
+        except pika.exceptions.AMQPConnectionError:
+            raise MessageMiddlewareDisconnectedError()
+        except Exception as e:
+            raise MessageMiddlewareMessageError() from e
+
+    def start_consuming(self, on_message_callback):
+        def callback(channel, method, properties, body):
+            ack = lambda: channel.basic_ack(delivery_tag=method.delivery_tag)
+            nack = lambda: channel.basic_nack(delivery_tag=method.delivery_tag)
+            on_message_callback(body, ack, nack)
+
+        try:
+            self.channel.basic_qos(prefetch_count=1)
+            self.channel.basic_consume(
+                queue=self.queue_name, on_message_callback=callback
+            )
+            self.channel.start_consuming()
+        except pika.exceptions.AMQPConnectionError:
+            raise MessageMiddlewareDisconnectedError()
+        except Exception as e:
+            raise MessageMiddlewareMessageError() from e
+
+    def stop_consuming(self):
+        try:
+            self.channel.stop_consuming()
+        except pika.exceptions.AMQPConnectionError:
+            raise MessageMiddlewareDisconnectedError()
+        except Exception as e:
+            raise MessageMiddlewareMessageError() from e
+
+    def close(self):
+        try:
+            if self.connection and self.connection.is_open:
+                self.connection.close()
+        except pika.exceptions.AMQPConnectionError:
+            raise MessageMiddlewareDisconnectedError()
+        except Exception as e:
+            raise MessageMiddlewareCloseError() from e
+
+
+class MessageMiddlewareQueueRabbitMQ(_MessageMiddlewareRabbitMQBase, MessageMiddlewareQueue):
     def __init__(self, host, queue_name):
-        pass
+        super().__init__(host)
+        self.queue_name = queue_name
+        try:
+            self.channel.queue_declare(queue=queue_name, durable=True)
+        except pika.exceptions.AMQPConnectionError:
+            raise MessageMiddlewareDisconnectedError()
+        except Exception as e:
+            raise MessageMiddlewareMessageError() from e
 
-class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
-    
+    def send(self, message):
+        try:
+            self.channel.basic_publish(
+                exchange="",
+                routing_key=self.queue_name,
+                body=message,
+                properties=pika.BasicProperties(
+                    delivery_mode=pika.DeliveryMode.Persistent
+                ),
+            )
+        except pika.exceptions.AMQPConnectionError:
+            raise MessageMiddlewareDisconnectedError()
+        except Exception as e:
+            raise MessageMiddlewareMessageError() from e
+
+
+class MessageMiddlewareExchangeRabbitMQ(_MessageMiddlewareRabbitMQBase, MessageMiddlewareExchange):
     def __init__(self, host, exchange_name, routing_keys):
-        pass
+        super().__init__(host)
+        self.exchange_name = exchange_name
+        self.routing_keys = routing_keys
+        try:
+            self.channel.exchange_declare(
+                exchange=self.exchange_name, exchange_type="topic"
+            )
+
+            result = self.channel.queue_declare("", exclusive=True)
+            self.queue_name = result.method.queue
+
+            for routing_key in self.routing_keys:
+                self.channel.queue_bind(
+                    exchange=self.exchange_name,
+                    queue=self.queue_name,
+                    routing_key=routing_key,
+                )
+        except pika.exceptions.AMQPConnectionError:
+            raise MessageMiddlewareDisconnectedError()
+        except Exception as e:
+            raise MessageMiddlewareMessageError() from e
+
+    def send(self, message):
+        try:
+            for routing_key in self.routing_keys:
+                self.channel.basic_publish(
+                    exchange=self.exchange_name, routing_key=routing_key, body=message
+                )
+        except pika.exceptions.AMQPConnectionError:
+            raise MessageMiddlewareDisconnectedError()
+        except Exception as e:
+            raise MessageMiddlewareMessageError() from e
